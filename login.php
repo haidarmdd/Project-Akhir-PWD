ea9d78176eeabc693fa15dac2f5ed52e806004c0
haidarmdd <124250029@student.upnyk.ac.id>
2026-04-29 20:50:44 +0700

    halaman login

---
+++ login.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Task Management</title>
+</head>
+<body>
+     <h2>LOGIN</h2>
+
+     <form action="POST" action="proses_login.php">
+        <input type="text" name="username" placeholder="Username" required><br><br>
+        <input type="password" name="password" placeholder="Password" required><br><br>
+        <button type="submit">LOGIN</button>
+     </form>
+        
+    <P>Belum memiliki akun? <a href="daftar.php">Daftar disini</a></P>
+    
+</body>
+</html>
