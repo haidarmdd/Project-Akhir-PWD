eb82f4819f87f73dfaa482104193f0e55c2052d3
haidarmdd <124250029@student.upnyk.ac.id>
2026-04-29 20:31:13 +0700

    upload pertama

---
+++ koneksi_db.php
@@ -0,0 +1,14 @@
+<?php
+    $hostname = "localhost";
+    $username = "root";
+    $password = "";
+    $database = "deadline_tracker";
+
+    $konek = new mysqli($hostname, $username, $password, $database);
+
+    if ($konek->connect_error) {
+        die("Maaf koneksi gagal" . $konek->connect_error);
+    } else {
+         echo "Koneksi berhasil";
+    }
+?>
