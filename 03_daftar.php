c8d83499e964d6a0b85c2286058e650e8bfc4e46
dapaapcb <daffaabyanacc4study@gmail.com>
2026-05-03 18:21:05 +0700

    refactor: rename files with numbering

--- 03_daftar.php
+++ 03_daftar.php
@@ -8,10 +8,10 @@
 <body>
      <h2>Silahkan Daftar Untuk Melanjutkan</h2>
 
-     <form action="POST" action="proses_login.php">
-        <input type="text" name="Nama Lengkap" placeholder="Nama Lengkap" required><br><br>
+     <form action="POST" action="proses_Daftar.php">
+        <input type="text" name="Nama_Lengkap" placeholder="Nama Lengkap" required><br><br>
         <input type="text" name="NIM" placeholder="NIM" required><br><br>
-        <input type="Password" name="Password" placeholder="Password" required><br><br>
+        <input type="password" name="Password" placeholder="Password" required><br><br>
         <input type="Password" name="Konfirmasi Password" placeholder="Konfirmasi Password" required><br><br>
         <button type="submit">Daftar</button>
      </form>
