c98ba9762d904b14e03d5a04b729791d50761299
dapaapcb <daffaabyanacc4study@gmail.com>
2026-05-12 18:40:15 +0700

    index

--- 02_index.php
+++ 02_index.php
@@ -1,11 +1,24 @@
+<?php
+session_start();
+if (isset($_SESSION['user_id'])) {
+    header("Locatiom: 05_dashboard.php");
+    exit;
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Index</title>
+    <title>Deadline Tracker</title>
 </head>
 <body>
-    
+    <h1>Deadline Tracker</h1>
+    <p>Kelola deadline tugas dan ujian kuliahmu</p>
+    <hr>
+    <p>
+        <a href="login.php">Login</a>
+        <a href="daftar.php">Daftar Akun Baru</a>
+    </p>
 </body>
 </html>
